6f43e5d5ec9f2ce6e5513d6d081e753bad00313b
xufangx8 <47289736+xufangx8@users.noreply.github.com>
2019-10-09 20:12:44 +0900

    Delete top_controller.rb

--- top_controller.rb
+++
@@ -1,6 +0,0 @@
-class TopController < ApplicationController
-  def index 
-    @articles = Article.visible.order(released_at: :desc).limit(5)
-    @articles = Article.open_to_the_public unless current_member
-  end
-end
